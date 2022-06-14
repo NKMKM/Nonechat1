3e8ad39a7bf481e7dcec67124a163865c5389c1b
NKMKM <77494915+NKMKM@users.noreply.github.com>
2022-06-14 17:39:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SetAvatar from "./components/SetAvatar";
 import Chat from "./pages/Chat";
@@ -6,11 +6,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";  
 
 export default function App() {
-  useEffect(() => {
-fetch("https://nonechat1.herokuapp.com/")
-    .then((res) => res.json())
-    .the((data) => console.log(data));
-  },[]);
+ 
   return (
     <BrowserRouter>
       <Routes>
